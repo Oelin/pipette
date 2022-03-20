abfc53c23cf7c8ee82e08ea261453538dd25c957
Oelin <42823429+Oelin@users.noreply.github.com>
2022-03-20 09:55:40 +0000

    Create pipette.py

---
+++ pipette.py
@@ -0,0 +1,65 @@
+# pipery, a minimal functional pipelining framework. 
+
+
+from functools import reduce
+
+
+def then(function):
+    
+    def ret(iterable):
+        
+        for value in iterable:
+            try:
+                yield function(value) or value
+            except: pass
+    
+    return ret
+
+
+def filter(predicate):
+    
+    def ret(iterable):
+        for value in iterable:
+            if predicate(value): yield value
+    
+    return ret
+
+
+def reduce(function):
+    
+    def ret(iterable):
+        result = next(iterable)
+        
+        for value in iterable:
+            result = function(result, value)
+        
+        yield result
+    
+    return ret
+
+
+def serialise(iterator):
+    
+    def ret(iterable):
+        
+        for value in iterable:
+            for nested in iterator(value):
+                yield nested
+    
+    return ret
+
+
+def collect(function):
+    
+    def ret(iterable):
+        yield function(list(iterable))
+    
+    return ret
+
+
+def connect(*iterators):
+    
+    def ret(iterable):
+        return reduce(lambda a, b: b(a), iterators, iterable)
+    
+    return ret
